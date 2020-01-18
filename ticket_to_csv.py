2d87679378edbb278bfeee294de25b7e22926e5a
Will Fenton <31602935+willfenton@users.noreply.github.com>
2020-01-18 14:19:16 -0700

    Fixed sample data (#36)

--- ticket_to_csv.py
+++ ticket_to_csv.py
@@ -9,6 +9,7 @@ def preprocess_text(text):
     text = text.replace(",", "")  # remove commas
     text = text.replace('"', "")  # remove double quotes
     text = text.replace("\n", "")  # remove newlines
+    text = text.replace("\r", "")  # remove other newlines
     return text
 
 
