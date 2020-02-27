25b6e39341d2c567f3a470895b1505c7c7c80bec
Yuan Wang <wang17@ualberta.ca>
2020-02-26 20:05:35 -0700

    format the code

--- canosp2020/preprocessing.py
+++ canosp2020/preprocessing.py
@@ -42,10 +42,10 @@ class Preprocess:
         # return None for now
         if doc.detect_language()[1] != "en":
             # do some manual regex preprocessing and re-try
-            text = text.strip() #remove whitespaces in the front and the end
+            text = text.strip()  # remove whitespaces in the front and the end
             text = re.sub("</?.*?>", " <> ", doc.raw)  # remove tags
             text = re.sub("(\\d|\\W)+", " ", text)  # remove special chars and digits
-            text = re.sub(r'[.|,|)|(|\|/|?|!|\'|"|#]',r' ',text) # remove any punctuation
+            text = re.sub(r'[.|,|)|(|\|/|?|!|\'|"|#]', r" ", text)  # remove any punctuation
             doc = Doc(raw=text)
             if doc.detect_language()[1] != "en":
                 print("Cannot determine input language.")
